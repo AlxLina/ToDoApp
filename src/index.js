8f738f719822b35bb0059afad10d474da063628d
Alina <kotmarsik2001.ka@gmail.com>
2023-10-09 22:43:22 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const addButton = document.querySelector(".add_plan");
 const inputText = document.querySelector(".input_plan");
 const createdPlan = document.querySelector(".created_plans");
 
-
 //events 
 addButton.addEventListener("click", () => {
     AddDelPlan();
@@ -20,6 +19,8 @@ function AddDelPlan(){
     plan.classList.add("new_plan");
     plan.innerHTML = `
     <p class="text_plan">${inputText.value}</p>`;
+
+    const yourinput = inputText.value;
 
     const text = document.createElement("p");
     text.classList.add("text_plan");
@@ -44,27 +45,86 @@ function AddDelPlan(){
 
     deleteButton.addEventListener("click", () =>{
         plan.remove();
-        // SaveProgress();
+        DeleteProgress(yourinput);
     });
 
-    check.addEventListener("click", () =>{
-        // SaveProgress();
-    });
-
-    // SaveProgress();
+    SaveProgress(yourinput);
 };
 
-// //save progress in storage
-// function SaveProgress() {
-//     localStorage.setItem("data", createdPlan.${inputText.value});
-// };
-// function ShowPlan(){
-//     createdPlan.innerHTML = localStorage.getItem("data");
-// };
+// func save progress in storage
+function SaveProgress(value){
+    if (localStorage.getItem('key')){
+    let tasks = localStorage.getItem('key');
+    const arr = tasks.split(',');
+    arr.push(value);
+    console.log(arr);   
+    localStorage.setItem('key', arr);
+    } else {
+        let arr1 = [];
+        arr1.push(value);
+        localStorage.setItem('key', arr1);
+    };
+};
 
-// //for deleting items after upload page (but only first)
-// ShowPlan();
+// func delete tasks from storage
+function DeleteProgress(value){
+    let tasks = localStorage.getItem('key');
+    const arr = tasks.split(',');
+    for (i = 0; i < arr.length; i++){
+        if (arr[i] === value){
+            arr.splice(i, 1);
+            break
+        };
+    };
+    localStorage.setItem('key', arr);
+};
 
+// func show tasks from storage
+function ShowPlan(){
+    if (localStorage.getItem('key')){
+        tasks = localStorage.getItem('key');
+        const arr = tasks.split(',');
+        for (i = 0; i < arr.length; i++){
+            appendPlan(arr[i]);
+        };
+    };
+};
 
+// show tasks when reload page
+ShowPlan();
 
+//func create plan from localstorage
+function appendPlan(value){
+    const plan = document.createElement("div");
+    plan.classList.add("new_plan");
+    plan.innerHTML = `
+    <p class="text_plan">${value}</p>`;
 
+    const yourinput = inputText.value;
+
+    const text = document.createElement("p");
+    text.classList.add("text_plan");
+
+    const box = document.createElement("div");
+    box.classList.add("box_action");
+
+    const check = document.createElement("input");
+    check.classList.add("done");
+    check.type = "checkbox";
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete");
+    deleteButton.innerHTML = `<img src="../img/trash.png" alt="trash">`;
+
+    box.appendChild(check);
+    box.appendChild(deleteButton);
+    plan.appendChild(box);
+    createdPlan.appendChild(plan);
+
+    inputText.value = "";
+
+    deleteButton.addEventListener("click", () =>{
+        plan.remove();
+        DeleteProgress(value);
+    });
+};
